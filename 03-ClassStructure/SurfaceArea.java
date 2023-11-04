90fad1361104ddd5e927833c30a24fdfb5d999c6
weronikaglazer <weronkaglazer@gmail.com>
2023-11-04 12:04:58 +0100

    Tasks 4-6

---
+++ 03-ClassStructure/SurfaceArea.java
@@ -0,0 +1,7 @@
+public class SurfaceArea {
+    static float rectangle(float a, float b){
+        return a*b;
+    }
+}
+
+
---
+++ 03-ClassStructure/SurfaceAreaTest.java
@@ -0,0 +1,6 @@
+public class SurfaceAreaTest {
+    public static void main(String[] args) {
+        System.out.print("Rectangle area (5,2) is ");
+        System.out.println(rectangle(5,2));
+    }
+}
